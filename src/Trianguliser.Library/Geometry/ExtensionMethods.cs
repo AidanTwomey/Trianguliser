39d04487a236d986ff6d7ffa6790a846d0508229
Aidan Twomey <aidan@specialv.co.uk>
2020-04-26 08:12:42 +0100

    simple turn right case

--- src/Trianguliser.Library/Geometry/ExtensionMethods.cs
+++ src/Trianguliser.Library/Geometry/ExtensionMethods.cs
@@ -2,5 +2,9 @@ namespace aidantwomey.Trianguliser.Library.Geometry
 {
     public static class ExtensionMethods
     {
+        public static bool TurnsRight(this DirectedLineSegment l1, DirectedLineSegment l2)
+        {
+            return l1.Slope > l2.Slope;
+        }
     }
 }
--- tests/Trianguliser.Library.Tests/Given_A_Pair_Of_DirectedLineSegments.cs
+++ tests/Trianguliser.Library.Tests/Given_A_Pair_Of_DirectedLineSegments.cs
@@ -1,29 +1,48 @@
 using aidantwomey.Trianguliser.Library.Geometry;
+using Shouldly;
 using Xunit;
 
 namespace aidantwomey.Trianguliser.Library.Tests
 {
     public class Given_A_Pair_Of_DirectedLineSegments
     {
-        public static TheoryData<DirectedLineSegment, DirectedLineSegment> Data
+        public static TheoryData<DirectedLineSegment, DirectedLineSegment> TurnRightData
         {
             get
             {
                 var data = new TheoryData<DirectedLineSegment, DirectedLineSegment>();
-                // data.Add(new Point(-1.0, 1.0), new Point(2.0, 2.0), 1.0/3.0);
-                // data.Add(new Point(1.0, 1.0), new Point(4.0, 2.0), 1.0/3.0);
-                // data.Add(new Point(1.0, 1.0), new Point(2.0, 2.0), 1.0);
-                // data.Add(new Point(1.0, 2.0), new Point(2.0, 1.0), -1.0);
-                // data.Add(new Point(1.0, 1.0), new Point(2.0, 1.0), 0);
-                // data.Add(new Point(1.0, 2.0), new Point(1.0, 1.0), double.PositiveInfinity);
+                data.Add(
+                    new DirectedLineSegment(new Point(-1.0, 1.0), new Point(2.0, 2.0)),
+                    new DirectedLineSegment(new Point(2.0, 2.0), new Point(5.0, 2.0)));
                 return data;
             }
         }
 
-        [Fact]
-        public void When_First_Segment_Points_Up_And_Second_Segment_Has_Lower_Slope_Then_TurnRight_Returns_True()
+        public static TheoryData<DirectedLineSegment, DirectedLineSegment> TurnLeftData
         {
-            
+            get
+            {
+                var data = new TheoryData<DirectedLineSegment, DirectedLineSegment>();
+                data.Add(
+                    new DirectedLineSegment(new Point(-1.0, 1.0), new Point(2.0, 2.0)),
+                    new DirectedLineSegment(new Point(2.0, 2.0), new Point(5.0, 6.0)));
+                return data;
+            }
+        }
+
+        
+        [Theory]
+        [MemberData(nameof(TurnRightData))]
+        public void When_First_Segment_Points_Up_And_Second_Segment_Has_Lower_Slope_Then_TurnRight_Returns_True(DirectedLineSegment upSegment, DirectedLineSegment nextSegment)
+        {
+            upSegment.TurnsRight(nextSegment).ShouldBeTrue();
+        }
+
+        [Theory]
+        [MemberData(nameof(TurnLeftData))]
+        public void When_First_Segment_Points_Up_And_Second_Segment_Has_Greater_Slope_Then_TurnRight_Returns_False(DirectedLineSegment upSegment, DirectedLineSegment nextSegment)
+        {
+            upSegment.TurnsRight(nextSegment).ShouldBeFalse();
         }
     }
 }
